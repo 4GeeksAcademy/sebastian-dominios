100bdb6ba02e27fced03691cd75c6892253f9b16
Sebaperez12 <sebaperez12@gmail.com>
2025-10-23 13:39:15 +0000

    dominio sebastian ajustes 2

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let pronouns = ['the', 'our'];
 let adjs = ['great', 'big' ];
 let nouns = ['jogger', 'racoon', "geeks"];
 
-
 for (let i = 0; i < pronouns.length; i++) {
   for (let j = 0; j < adjs.length; j++) {
     for (let h = 0; h < nouns.length; h++) {
